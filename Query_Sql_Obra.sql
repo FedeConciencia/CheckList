1ab264c50b3e457f8d38dbf353c24213bbfc8ccc
Federico.Sabatini <federico.sabatini@gmail.com>
2022-09-06 11:50:00 -0300

    Se crear el repositorio de proyecto full stack check list obra se agrega => Base de Datos MySql Normalizada, Servidor en Java y Cliente en ReactJs

---
+++ Query_Sql_Obra.sql
@@ -0,0 +1,10 @@
+create database proyecto_check_list;
+
+use proyecto_check_list;
+
+SET SQL_SAFE_UPDATES = 0;
+
+alter table dinosaurio auto_increment = 1;
+
+
+                                                                                                                                                     
